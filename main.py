11a3c3c56a74298c1e3b5b9b82a7aca645a5195d
Arthur Klassen <klassenorg@gmail.com>
2020-10-15 20:53:01 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+
+import logging
+import time
+from selenium import webdriver
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import records
+import creds
+
+# Enable logging
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+
+#Error handler
+def error(bot, update, error):
+    logger.warn('Update "%s" caused error "%s"' % (update, error))
+
+
+#initiate DB
+def initdb(env):
+    if env == 'prod':
+        database = records.Database(creds.ATGPRD)
+        return database
+    else:
+        database = records.Database(creds.ATGPLT)
+        return database
+    return database
+
+def get_external_order_id(order_id):
+    db = initdb('prod')
+    rows = db.query("select payment_id, external_order_id from prod_production.mvid_sap_order_vtb_payment where payment_id = " + chr(39) + str(order_id) + chr(39))
+    if rows.one() is None:
+        db = initdb('pilot')
+        rows = db.query("select payment_id, external_order_id from pilot_production.mvid_sap_order_vtb_payment where payment_id = " + chr(39) + str(order_id) + chr(39))
+        rows = rows.as_dict()[0]
+        return rows['external_order_id']
+    else:
+        rows = rows.as_dict()[0]
+        return rows['external_order_id']
+
+options = webdriver.ChromeOptions()
+options.add_argument('headless')
+
+def getOrderFromVTB(update, context):
+    """Send VTB order status"""
+    order_id = ''.join(context.args)
+    ext_order_id = get_external_order_id(order_id)
+    driver = webdriver.Chrome("C:/Users/klass/Downloads/chromedriver.exe", options=options)
+    driver.get('https://platezh.vtb24.ru/mportal/#login')
+    username = driver.find_element_by_id("username-inputEl")
+    username.click()
+    username.send_keys(creds.vtb_login)
+    password = driver.find_element_by_id("password-inputEl")
+    password.click()
+    password.send_keys(creds.vtb_password)
+    time.sleep(1)
+    submit = driver.find_element_by_id("button-1117-btnInnerEl")
+    submit.click()
+    time.sleep(1)
+    driver.get("https://platezh.vtb24.ru/mportal/#orders/" + ext_order_id + "/history?orderNumber=" + order_id)
+    time.sleep(1)
+    table = driver.find_elements_by_class_name("x-grid-cell-paymentState")
+    paymentStatus = table[-1].text
+    update.message.reply_text(paymentStatus)
+    driver.close()
+
+
+def main():
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
+    updater = Updater(creds.bot_api, use_context=True)
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("vtb", getOrderFromVTB))
+
+    # on noncommand i.e message - echo the message on Telegram
+    #dp.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
